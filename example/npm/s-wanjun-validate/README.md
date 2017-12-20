bad06b8233298cd108912c4dfa887a525436587b
joeny <315432612@qq.com>
2017-12-20 14:23:46 +0800

    -

---
+++ example/npm/s-wanjun-validate/README.md
@@ -0,0 +1,30 @@
+
+# API
+
+- isArray()
+
+- isObject()
+
+- isString()
+
+- isDate()
+
+- isRegExp()
+
+- isFunction()
+
+- isBoolean()
+
+- isNumber()
+
+- isNull()
+
+- isUndefined()
+
+# 示例
+
+```
+var $ = require('s-wanjun-validate')
+console.log('funtion=>', $.isFunction(() => {}))
+console.log('array=>', $.isArray([]))
+```
---
+++ example/npm/s-wanjun-validate/index.js
@@ -0,0 +1,23 @@
+var $  = {}
+
+$ = (function () {
+  'use strict'
+
+  var types = 'Array Object String Date RegExp Function Boolean Number Null Undefined'.split(' ')
+
+  function type () {
+    return Object.prototype.toString.call(this).slice(8, -1)
+  }
+
+  for (var i = 0; i < types.length; i ++) {
+      $['is' + types[i]] = (function (self) {
+          return function (elem) {
+            return type.call(elem) === self
+          }
+      })(types[i])
+  }
+
+  return $
+})()
+
+module.exports = $
